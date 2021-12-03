5fb0412d05e4fbc80eaa81d432c9cce6158f1cdd
Mirko <bitettomirkp@gmail.com>
2021-12-03 10:50:41 +0100

    first commit

---
+++ farmer.c
@@ -0,0 +1,53 @@
+/* 
+ * Operating Systems  (2INCO)  Practical Assignment
+ * Interprocess Communication
+ *
+ * STUDENT_NAME_1 (STUDENT_NR_1)
+ * STUDENT_NAME_2 (STUDENT_NR_2)
+ *
+ * Grading:
+ * Your work will be evaluated based on the following criteria:
+ * - Satisfaction of all the specifications
+ * - Correctness of the program
+ * - Coding style
+ * - Report quality
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>    
+#include <unistd.h>    // for execlp
+#include <mqueue.h>    // for mq
+
+
+#include "settings.h"  // definition of work
+#include "common.h"
+
+
+int main (int argc, char * argv[])
+{
+    if (argc != 1)
+    {
+        fprintf (stderr, "%s: invalid arguments\n", argv[0]);
+    }
+        
+    // TODO:
+    //  * create the message queues (see message_queue_test() in
+    //    interprocess_basic.c)
+    //  * create the child processes (see process_test() and
+    //    message_queue_test())
+    //  * do the farming
+    //  * wait until the chilren have been stopped (see process_test())
+    //  * clean up the message queues (see message_queue_test())
+
+    // Important notice: make sure that the names of the message queues
+    // contain your student name and the process id (to ensure uniqueness
+    // during testing)
+    
+    return (0);
+}
