784d3d6a255b26bba8abaed75975f5fb73b3ad98
Mirko <bitettomirkp@gmail.com>
2021-12-03 12:40:32 +0100

    Creation and cleanup of MQ implemented in farmer.c

--- common.h
+++ common.h
@@ -9,13 +9,22 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-
 // Maximum size for any message in the tests
-#define MAX_MESSAGE_LENGTH	6
- 
+#define MAX_MESSAGE_LENGTH 6
 
 // TODO: put your definitions of the datastructures here
+typedef struct
+{
 
+    // TODO: define data structure of the request message
+
+} MQ_REQUEST_MESSAGE;
+
+typedef struct
+{
+
+    // TODO: define data structure of the response message
+
+} MQ_RESPONSE_MESSAGE;
 
 #endif
-
--- farmer.c
+++ farmer.c
@@ -20,34 +20,66 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <errno.h>    
-#include <unistd.h>    // for execlp
-#include <mqueue.h>    // for mq
+#include <errno.h>
+#include <unistd.h> // for execlp
+#include <mqueue.h> // for mq
 
-
-#include "settings.h"  // definition of work
+#include "settings.h" // definition of work
 #include "common.h"
 
+static char mq_name1[80]; // definition of the message queque name
+static char mq_name2[80];
 
-int main (int argc, char * argv[])
+#define STUDENT_NAME "TEST" // just added as a test, needs to be changed later
+
+int main(int argc, char *argv[])
 {
     if (argc != 1)
     {
-        fprintf (stderr, "%s: invalid arguments\n", argv[0]);
+        fprintf(stderr, "%s: invalid arguments\n", argv[0]);
     }
-        
+
     // TODO:
     //  * create the message queues (see message_queue_test() in
     //    interprocess_basic.c)
+
+    pid_t processID[NROF_WORKERS]; /* Array of proces ID from fork() */
+
+    // i took the following code from interproces_basics.c
+    mqd_t mq_fd_request;
+    mqd_t mq_fd_response;
+    MQ_REQUEST_MESSAGE req;
+    MQ_RESPONSE_MESSAGE rsp;
+    struct mq_attr attr;
+
+    sprintf(mq_name1, "/mq_request_%s_%d", STUDENT_NAME, getpid());
+    sprintf(mq_name2, "/mq_response_%s_%d", STUDENT_NAME, getpid());
+
+    attr.mq_maxmsg = MQ_MAX_MESSAGES;
+    attr.mq_msgsize = sizeof(MQ_REQUEST_MESSAGE);
+    mq_fd_request = mq_open(mq_name1, O_WRONLY | O_CREAT | O_EXCL, 0600, &attr);
+
+    attr.mq_maxmsg = MQ_MAX_MESSAGES;
+    attr.mq_msgsize = sizeof(MQ_RESPONSE_MESSAGE);
+    mq_fd_response = mq_open(mq_name2, O_RDONLY | O_CREAT | O_EXCL, 0600, &attr);
+
     //  * create the child processes (see process_test() and
     //    message_queue_test())
+
     //  * do the farming
+
     //  * wait until the chilren have been stopped (see process_test())
+
     //  * clean up the message queues (see message_queue_test())
+
+    mq_close(mq_fd_response);
+    mq_close(mq_fd_request);
+    mq_unlink(mq_name1);
+    mq_unlink(mq_name2);
 
     // Important notice: make sure that the names of the message queues
     // contain your student name and the process id (to ensure uniqueness
     // during testing)
-    
+
     return (0);
 }
